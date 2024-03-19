462c6b51624072b8cc25affe26076ee9a9f55034
marl0t <doikovmaksim@gmail.com>
2024-03-19 21:55:54 +0800

    all dark theme and navbar

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-document.getElementById('themeToggle').addEventListener('click', function () {
-    const currentTheme = document.body.className;
-    if (currentTheme === 'light-theme') {
-        document.body.className = 'dark-theme';
-    } else {
-        document.body.className = 'light-theme';
-    }
-});
+window.onload = function () {
+    let body = document.getElementsByTagName('body')[0];
+    let sunBtn = document.getElementById('sunBtn');
+    let moonBtn = document.getElementById('moonBtn');
+
+    document.getElementById('themeToggle').addEventListener('click', function () {
+        if (body.classList.contains('dark-theme')) {
+
+            body.classList.add("light-theme");
+            body.classList.remove("dark-theme");
+            sunBtn.classList.remove("d-none");
+            moonBtn.classList.add("d-none");
+        }
+        else {
+            body.classList.add("dark-theme");
+            body.classList.remove("light-theme");
+            sunBtn.classList.add("d-none");
+            moonBtn.classList.remove("d-none");
+        }
+
+    });
+}
