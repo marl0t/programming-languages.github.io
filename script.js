78de8d5f66f221ad3bcf31011e5fc966431b7a5a
marl0t <doikovmaksim@gmail.com>
2024-03-19 19:00:45 +0800

    темная тема и пару штук

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('themeToggle').addEventListener('click', function () {
+    const currentTheme = document.body.className;
+    if (currentTheme === 'light-theme') {
+        document.body.className = 'dark-theme';
+    } else {
+        document.body.className = 'light-theme';
+    }
+});
